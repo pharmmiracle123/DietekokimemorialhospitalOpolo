b0f4ef693689971895d05f0e174591356e561994
pharmmiracle123 <onyekamiracle08@gmail.com>
2024-11-28 16:32:26 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,29 @@
+<DOCTYPE html>
+<html>
+  <head>
+    <title>DKMH</title>
+    <link rel= "stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootsrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-info bg-info">
+    <h5 class="text-white">Deite-koki Memorial Hospital</h5>
+    <div class="mr-auto">
+    
+    </div>
+    <ul class="navbar-nav">
+      <li class="nav-items">
+        <a href="" class="nav-link">Admin</a>
+      </li>
+      <li class="nav-items">
+        <a heref="" class="nav-link">Doctors</a>
+      </li>
+      <li class="nav-items">
+        <a heref="" class="nav-link">Patients</a>
+      </li>
+    </ul>
+  </nav>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,42 @@
+<DOCTYPE html>
+  <html>
+    <head>
+      <tile>
+        Deito-Koki Memorial Hospital 
+      </tile>
+    </head>
+    <body>
+      <?php
+      include("include/header.php");
+     ?>
+     
+     <div class="container">
+       <div class="col-md-12">
+         <div class="row">
+           <div class="col-md-3 mx-1 shadow">
+             <img scr="#" style="width:#%" height:>
+              <h5 class="text-center">Click on the button fir more information</h5>
+             <a href="">
+               <button class="btn btn-success my-3" style="margin left:30%;">More Information</button>
+             </a>
+           </div>
+           <div class="col-md-4 mx-1 shadow">
+             <img scr="#" width:#%;>
+              <h5 class="text-center">Create an account so thaf we can take good care of you</h5>
+             <a href="">
+               <button class="btn btn-success my-3" style="margin left:30%;">Create Account</button>
+             </a>
+           </div>
+           
+           <div class="col-md-4 mx-1 shadow">
+             <img scr="#" width:#%;>
+             <h5 class="text-center">Doctors</h5>
+             <a href="">
+               <button class="btn btn-success my-3" style="margin left:30%;">View List of Doctors</button>
+             </a>
+           </div>
+         </div>
+       </div>
+     </div>
+    </body>
+  </html>
